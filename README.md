1e3e93396110e873558e1a87d3fab8553487c757
revadisundaram7 <sundaramrevadi@gmail.com>
2024-10-16 09:33:48 +0530

    Committing max occurence

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # DSA
+# Finding number of occurence of a string
+- Hello - l 2 times
+```js
+function countOccurence(str){
+    let obj = {};
+    let maxEl = 0;
+    let keyEl = '';
+    for(let i=0; i<str.length; i++){
+        if(str[i] in obj){
+            obj[str[i]] ++;
+        }
+        else{
+            obj[str[i]] = 1
+        }
+        if(maxEl < obj[str[i]]){
+            maxEl = obj[str[i]]
+            keyEl = str[i]
+        }
+    }
+    return [maxEl, keyEl]
+}
+console.log(countOccurence(string));
+```
---
+++ script.js
@@ -0,0 +1,41 @@
+//Finding number of occurence of a string
+let string = "Renu";
+function countOccurence(str){
+    let obj = {};
+    let maxEl = 0;
+    let keyEl = '';
+    for(let i=0; i<str.length; i++){
+        if(str[i] in obj){
+            obj[str[i]] ++;
+            // if(maxEl < obj[str[i]]){
+            //     maxEl = obj[str[i]]
+            //     keyEl = str[i]
+            // }
+        }
+        else{
+            obj[str[i]] = 1
+        }
+        if(maxEl < obj[str[i]]){
+            maxEl = obj[str[i]]
+            keyEl = str[i]
+        }
+    }
+    return [maxEl, keyEl]
+    // let arr =  findMax(obj);
+    // return arr
+}
+console.log(countOccurence(string));
+// function findMax(obj){
+//     let maxEl = 0;
+//     let keyEl = '';
+//     for(let key in obj){
+//         console.log(key, obj[key]);
+//         if(maxEl < obj[key]){
+//             maxEl = obj[key]
+//             keyEl = key
+            
+//         }
+//     }
+//     // console.log("Max Element and key" , maxEl, keyEl);
+//     return [maxEl,keyEl]
+// }
